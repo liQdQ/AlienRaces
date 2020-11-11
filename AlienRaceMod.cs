22d1ad20de966eab55756bb6ab6d8bc8004569eb
liQdQ <74311003+liQdQ@users.noreply.github.com>
2020-11-11 19:33:01 +0100

    Add files via upload

---
+++ AlienRaceMod.cs
@@ -0,0 +1,46 @@
+﻿namespace AlienRace
+{
+    using RimWorld;
+    using UnityEngine;
+    using Verse;
+
+    public class AlienRaceMod : Mod
+    {
+        public static AlienRaceSettings settings;
+
+        public override string SettingsCategory() => "Alien Race";
+
+        public AlienRaceMod(ModContentPack content) : base(content) => 
+            settings = this.GetSettings<AlienRaceSettings>();
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled(label: "Use central melanin for factions", ref settings.centralMelanin, tooltip: "True: Pawns of the same factions will have more or less the same skin color.\nFalse: Skin color is not bound by factions.\nNote: Race authors may decide to override skin colors.");
+            listingStandard.End();
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            settings.UpdateSettings();
+        }
+    }
+
+    public class AlienRaceSettings : ModSettings
+    {
+        public bool centralMelanin;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.centralMelanin, label: "centralMelanin", defaultValue: false);
+        }
+
+        public void UpdateSettings() =>
+            ((ThingDef_AlienRace)ThingDefOf.Human).alienRace.generalSettings.alienPartGenerator.colorChannels.Find(match: ccg => ccg.name == "skin").first = 
+            this.centralMelanin ? null : new ColorGenerator_SkinColorMelanin { maxMelanin = 1f, minMelanin = 0f };
+    }
+}
---
+++ ColorGenerators.cs
@@ -0,0 +1,23 @@
+﻿namespace AlienRace
+{
+    using RimWorld;
+    using UnityEngine;
+    using Verse;
+
+    public class ColorGenerator_SkinColorMelanin : ColorGenerator
+    {
+        public float minMelanin = 0f;
+        public float maxMelanin = 1f;
+
+        public override Color NewRandomizedColor() => 
+            PawnSkinColors.GetSkinColor(Rand.Range(this.minMelanin, this.maxMelanin));
+    }
+
+    public class ColorGenerator_CustomAlienChannel : ColorGenerator
+    {
+        public string colorChannel;
+
+        public override Color NewRandomizedColor() => 
+            Color.clear;
+    }
+}
